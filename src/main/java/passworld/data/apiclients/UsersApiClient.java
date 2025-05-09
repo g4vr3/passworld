d6a3185154169db8018b6e61001a2fe6bd887d83
javier-agudo-fernandez <javier.agudo1@educa.madrid.org>
2025-05-09 13:08:57 +0200

    Refactor user registration to return hashed master password from remote if the user is logging in

--- src/main/java/passworld/data/apiclients/UsersApiClient.java
+++ src/main/java/passworld/data/apiclients/UsersApiClient.java
@@ -4,6 +4,7 @@ import jdk.jshell.spi.ExecutionControl;
 import okhttp3.*;
 import org.json.JSONObject;
 import passworld.data.exceptions.CredentialsException;
+import passworld.data.exceptions.EncryptionException;
 import passworld.data.session.UserSession;
 import passworld.utils.EncryptionUtil;
 
@@ -17,8 +18,8 @@ public class UsersApiClient {
     private static final String API_KEY = "AIzaSyB02VJOwdZp-QTf43icfVew7x0uNcdGEHE";
     private static final OkHttpClient client = new OkHttpClient();
 
-    public static void registerUserWithMasterPassword(String email, String password, String masterPassword)
-            throws Exception {
+    public static String registerUserWithMasterPassword(String email, String password, String masterPassword)
+            throws CredentialsException, IOException, EncryptionException {
         // 1. Registrar usuario
         String registerEndpoint = AUTH_BASE_URL + "signUp?key=" + API_KEY;
         String registerRequestBody = String.format("""
@@ -45,60 +46,72 @@ public class UsersApiClient {
         // 2. Iniciar sesión para obtener idToken
         loginUser(email, password);
         String idToken = UserSession.getInstance().getIdToken();
+        String hashedMasterPassword = EncryptionUtil.hashMasterPassword(masterPassword);
 
         // 3. Crear nodos en la base de datos
         String databaseEndpoint = DATABASE_BASE_URL + userId + ".json?auth=" + idToken;
         String databaseRequestBody = String.format("""
     {
-        "masterPassword": "%s",
-        "passwords": {
-                        "smplpass": {
-                          "description": "Sample description",
-                          "username": "sample",
-                          "password": "Sample1234##$$",
-                          "url": "https://mail.example.com",
-                          "isWeak": false,
-                          "isDuplicate": false,
-                          "isCompromised": false,
-                          "isUrlUnsafe": false,
-                          "lastModified": "2000-04-30T14:00:01"
-                        }
-        }
+        "masterPassword": "%s"
     }
-    """, EncryptionUtil.hashMasterPassword(masterPassword));
+    """, hashedMasterPassword);
 
         sendRequest(databaseEndpoint, "PUT", databaseRequestBody);
+        return hashedMasterPassword;
+
     }
 
     public static void loginUser(String email, String password)
             throws IOException, CredentialsException {
         String endpoint = "signInWithPassword?key=" + API_KEY;
         String requestBody = String.format("""
-        {
-            "email": "%s",
-            "password": "%s",
-            "returnSecureToken": true
-        }
-        """, email, password);
+{
+    "email": "%s",
+    "password": "%s",
+    "returnSecureToken": true
+}
+""", email, password);
 
-        String response;
-        try {
-            response = sendRequest(AUTH_BASE_URL + endpoint, "POST", requestBody);
-        } catch (IOException e) {
-            if (e.getMessage().contains("INVALID_PASSWORD") || e.getMessage().contains("EMAIL_NOT_FOUND")) {
+
+        String response = sendRequest(AUTH_BASE_URL + endpoint, "POST", requestBody);
+
+        JSONObject json = new JSONObject(response);
+        if (json.has("error")) {
+            String message = json.getJSONObject("error").getString("message");
+            System.out.println("Error de Firebase: " + message);
+            if (message.contains("INVALID_LOGIN_CREDENTIALS") || message.contains("EMAIL_NOT_FOUND") || message.contains("INVALID_EMAIL")) {
                 throw new CredentialsException("Credenciales incorrectas");
             } else {
-                throw e;
+                throw new IOException("Error de autenticación: " + message);
             }
         }
 
-        JSONObject json = new JSONObject(response);
         UserSession session = UserSession.getInstance();
         session.setLoggedIn(true);
         session.setUserId(json.getString("localId"));
         session.setIdToken(json.getString("idToken"));
         session.setRefreshToken(json.getString("refreshToken"));
+
     }
+    public static String fetchMasterPassword() throws IOException {
+        UserSession session = UserSession.getInstance();
+        String userId = session.getUserId();
+        String idToken = session.getIdToken();
+        String url = "https://passworld-g4vj4vi-default-rtdb.firebaseio.com/users/" + userId + "/masterPassword.json?auth=" + idToken;
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Error al recuperar la master password: " + response.message());
+            }
+            // El valor es un string plano (hash)
+            return response.body().string().replace("\"", "");
+        }
+    }
+
 
     // Método para refrescar el token
     public static void refreshIdToken(UserSession session) throws IOException {
@@ -130,12 +143,11 @@ public class UsersApiClient {
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            if (response.isSuccessful() && response.body() != null) {
-                return response.body().string();
-            } else {
-                String errorBody = response.body() != null ? response.body().string() : "Unknown error";
-                throw new IOException(errorBody);
+            String responseBody = response.body() != null ? response.body().string() : "";
+            if (!response.isSuccessful()) {
+                throw new IOException("Error en la solicitud HTTP: " + response.code() + " - " + responseBody);
             }
+            return responseBody;
         }
     }
 
